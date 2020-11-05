1da2036843bbea04691fe5a2c154e287b55b2b95
takahiro-sekine <doctype.html.1021@gmail.com>
2020-11-05 19:29:47 +0900

    ユーザー管理機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates first_name, presence: true
-  validates last_name, presence: true 
-  validates kana_first_name, presence: true        
-  validates kana_last_name, presence: true 
-  validates nickname, presence: true 
-  validates birthday, presence: true 
+  validates :first_name, presence: true
+  validates :last_name, presence: true 
+  validates :kana_first_name, presence: true        
+  validates :kana_last_name, presence: true 
+  validates :nickname, presence: true 
+  validates :birthday, presence: true 
  
 end
