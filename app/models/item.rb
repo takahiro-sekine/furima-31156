a45302d478b79d57711f8c7f7c384814e7eceea5
takahiro-sekine <doctype.html.1021@gmail.com>
2020-11-06 22:48:46 +0900

    カテゴリー選択の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belong_to :category
+  belong_to :burden
+  belong_to :shipping_day
+  belong_to :source
+  belong_to :status
+
+  validates :product_name, :explanation, :price presence: true
+  validates :category_id, :burden_id, :shipping_day_id, :source_id, :status_id, numericality: { other_than: 1 } 
 end
