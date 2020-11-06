0a804e4fbc8834de859ab69027e3efc8240d8c9d
takahiro-sekine <doctype.html.1021@gmail.com>
2020-11-06 15:46:28 +0900

    userモデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true 
-  validates :kana_first_name, presence: true        
-  validates :kana_last_name, presence: true 
+  validates :kana_first_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}       
+  validates :kana_last_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :nickname, presence: true 
   validates :birthday, presence: true 
- 
+  validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 end
