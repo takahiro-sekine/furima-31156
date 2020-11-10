2aaebb6947b799abd6cc3724786fa73a208ec6a3
takahiro-sekine <doctype.html.1021@gmail.com>
2020-11-10 18:44:01 +0900

    プルリクエスト改善２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @prototype.user_id
+    if current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
     else
