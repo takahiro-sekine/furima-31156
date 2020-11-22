92eb14174d96828a43e1ef84c9bb9395738a9c85
takahiro-sekine <doctype.html.1021@gmail.com>
2020-11-22 23:04:37 +0900

    js導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(address_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to action: :index
     else
@@ -18,19 +19,17 @@ class OrdersController < ApplicationController
   private
 
   def address_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id  )
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token] )
   end
 
-  def order_params
-    # params.require(:order).permit(:price).merge(token: params[:token])
-  end
+  
 
-  # def pay_item
-  # Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 自身のPAY.JPテスト秘密鍵を記述しましょう
-  # Payjp::Charge.create(
-  # amount: order_params[:price],  # 商品の値段
-  # card: order_params[:token],    # カードトークン
-  # currency: 'jpy'                 # 通貨の種類（日本円）
-  # )
-  # end
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+    amount: item_params[:price],  # 商品の値段
+    card: address_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+     )
+   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :municipalities
     validates :house_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1 }
 
