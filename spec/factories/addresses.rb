712a8896383cf1a8ae638da65c01483209250ca7
takahiro-sekine <doctype.html.1021@gmail.com>
2020-11-23 17:26:04 +0900

    show,indexの編集

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :address do
-    postal_code { 'MyString' }
-    prefectures_id { 1 }
-    municipalities { 'MyText' }
-    address { 'MyText' }
-    building_name { 'MyText' }
-    phone_number { 'MyString' }
-    purchase { '' }
+    postal_code { '321-2123' }
+    prefectures_id { 2 }
+    municipalities { '東京都' }
+    address { '渋谷区' }
+    building_name { '渋谷1−2−３' }
+    phone_number { '08080341234' }
   end
 end
